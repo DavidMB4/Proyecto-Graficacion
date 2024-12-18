873dee53b61da4eea5ccfa3010093a68685fb7f0
David Maldonado <david.maldonado.4810@gmail.com>
2024-12-17 20:21:08 -0600

    Correccion en corrales

--- Proyecto_graficacion.py
+++ Proyecto_graficacion.py
@@ -162,17 +162,6 @@ def poste_valla(texture):
     glTexCoord2f(0.0, 1.0); glVertex3f(-0.2, 0, 0.2)
     glEnd()
     
-def draw_tierra(texture):
-    glBindTexture(GL_TEXTURE_2D, texture)
-    glBegin(GL_QUADS)
-    glColor3f(1, 1, 1) 
-
-    glTexCoord2f(0.0, 0.0); glVertex3f(-5, 0.01, 5)
-    glTexCoord2f(1.0, 0.0); glVertex3f(5, 0.01, 5)
-    glTexCoord2f(1.0, 1.0); glVertex3f(5, 0.01, -5)
-    glTexCoord2f(0.0, 1.0); glVertex3f(-5, 0.01, -5)
-    glEnd()
-    
     
 def draw_corral_oveja(tierra_pasto_texture, madera_valla_texture):
     positionsH = [
@@ -219,7 +208,6 @@ def draw_corral_oveja(tierra_pasto_texture, madera_valla_texture):
         poste_valla(madera_valla_texture)
         glPopMatrix()
         
-    draw_tierra(tierra_pasto_texture)
     
 def draw_lodo(texture):
     glBindTexture(GL_TEXTURE_2D, texture)
@@ -277,8 +265,6 @@ def draw_corral_cerdo(tierra_pasto_texture, madera_valla_texture, lodo_texture):
         glTranslatef(*pos)   
         poste_valla(madera_valla_texture)
         glPopMatrix()
-        
-    draw_tierra(tierra_pasto_texture)
     
     glTranslatef(0.6, 0.03,  0)   
     draw_lodo(lodo_texture)
